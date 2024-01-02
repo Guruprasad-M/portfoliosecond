2e722d09b3c296098020f847a750270935e24926
unknown <guruprasadmariappan@gmail.com>
2024-01-02 11:26:38 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
-    const menu=document.querySelector(".menu-links");
-    const icon=document.querySelector(".handburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+    document.getElementById("menu-link").style.width = "70%";
 }
+function closeNav() {
+    document.getElementById("menu-link").style.width = "0";
+  }
